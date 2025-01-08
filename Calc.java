1b02e8d27aa1fe6924e37b8a2bed385c8d9af362
aman-khan-903 <amankhanvds22@gmail.com>
2025-01-08 10:40:55 +0530

    add function created

--- Calc.java
+++ Calc.java
@@ -1,4 +1,9 @@
 public class Calc{
+
+    double add(double a, double b){
+        return a+b; 
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the calc");
     }
