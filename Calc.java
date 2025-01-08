fd4bc41fbe4e42d9168aca69ec5b998f88fd427f
aman-khan-903 <amankhanvds22@gmail.com>
2025-01-08 10:42:35 +0530

    multiply function created

--- Calc.java
+++ Calc.java
@@ -1,4 +1,7 @@
 public class Calc{
+    double multiply(double a, double b){
+        return a*b; 
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the calc");
     }
