222c9ee14fb2dc16ad9e0810f3ff345a16ff03bc
aman-khan-903 <amankhanvds22@gmail.com>
2025-01-08 10:41:44 +0530

    subtract function created

--- Calc.java
+++ Calc.java
@@ -1,4 +1,8 @@
 public class Calc{
+    double subtract (double a, double b){
+        return a-b; 
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the calc");
     }
